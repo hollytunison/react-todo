9c833ecdda3d7f5a4702fac4db0fc96ae8c60092
Holly Tunison <hollytunison@gmail.com>
2023-02-16 09:19:09 -0500

    fixed delete button

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -15,7 +15,7 @@ export function Task({ task, onComplete, onDelete }) {
         {task.title}
       </p>
 
-      <button className={styles.deleteButton} onClick={() => onDelete=(task.id)}>
+      <button className={styles.deleteButton} onClick={() => onDelete(task.id)}>
         <TbTrash size={20} />
       </button>
     </div>
